c9c0c4873fbc76472146908683bb82c24fa7eab1
EstebanDSM <stephanedasil@gmail.com>
2021-10-22 21:24:54 +0200

    work on utils

--- app/src/main/java/com/example/realestatemanager/utils/Utils.java
+++ app/src/main/java/com/example/realestatemanager/utils/Utils.java
@@ -1,6 +1,7 @@
 package com.example.realestatemanager.utils;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
 import android.net.wifi.WifiManager;
 
 import java.text.DateFormat;
@@ -14,6 +15,15 @@ import java.util.Date;
 public class Utils {
 
     /**
+     * rate used to convert dollars to euros and euros to dollars
+     *
+     * @return
+     */
+    private static double getRate() {
+        return 0.859;
+    }
+
+    /**
      * Conversion d'un prix d'un bien immobilier (Dollars vers Euros)
      * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
      *
@@ -21,22 +31,15 @@ public class Utils {
      * @return
      */
     public static int convertDollarToEuro(int dollars) {
-        return (int) Math.round(dollars * 0.859);
+        return (int) Math.round(dollars * Utils.getRate());
     }
-
 
     /**
      * Conversion d'un prix d'un bien immobilier (Euros vers Dollars)
-     * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
-     *
-     * @param euros
-     * @return
      */
-
     public static int convertEurosToDollars(int euros) {
-        return (int) Math.round(euros * 1.164);
+        return (int) Math.round(euros / Utils.getRate());
     }
-
 
     /**
      * Conversion de la date d'aujourd'hui en un format plus approprié
@@ -45,19 +48,28 @@ public class Utils {
      * @return
      */
     public static String getTodayDate() {
-        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", java.util.Locale.getDefault());
         return dateFormat.format(new Date());
     }
 
     /**
-     * Vérification de la connexion réseau
+     * Vérification de la connexion réseau (seulement wifi)
      * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
      *
      * @param context
      * @return
      */
     public static Boolean isInternetAvailable(Context context) {
-        WifiManager wifi = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        WifiManager wifi = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         return wifi.isWifiEnabled();
+    }
+
+    /**
+     * Vérification complète de la connexion internet
+     */
+    public static boolean CheckInternetConnection(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isAvailable() && cm.getActiveNetworkInfo().isConnected();
     }
 }
