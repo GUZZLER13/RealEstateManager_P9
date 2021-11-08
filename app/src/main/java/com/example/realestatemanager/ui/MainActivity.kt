c85b0a1492c1dadc96e6263cd93cc04652a9c6e4
EstebanDSM <stephanedasil@gmail.com>
2021-11-08 14:50:14 +0100

    add toolbar layout

--- app/src/main/java/com/example/realestatemanager/ui/MainActivity.kt
+++ app/src/main/java/com/example/realestatemanager/ui/MainActivity.kt
@@ -2,14 +2,26 @@ package com.example.realestatemanager.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.realestatemanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mToolbar: Toolbar
+
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupToolbar()
+
+    }
+
+    fun setupToolbar() {
+        mToolbar = binding.materialToolbar
+        setSupportActionBar(mToolbar)
     }
 }
